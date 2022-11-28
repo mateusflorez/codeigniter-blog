04c39b45c07868c55263b3eec08e77f23c11804f
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-11-28 12:18:00 -0300

    navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,19 @@
 <html>
   <head>
     <title>CodeIgniter Blog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
   </head>
   <body>
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">CIBlog</a>
+      </div>
+      <div id="navbar">
+        <ul class="nav navbar-nav">
+          <li><a href="<?php echo base_url(); ?>">Home</a></li>
+          <li><a href="<?php echo base_url(); ?>about">About</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
