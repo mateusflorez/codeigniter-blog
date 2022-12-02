62504ed7297a34faaf64abe8fa67b342e030b1bf
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-12-02 11:30:47 -0300

    assets + navbar update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,17 +2,19 @@
   <head>
     <title>CodeIgniter Blog</title>
     <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+    <?php echo link_tag('assets/css/style.css'); ?>
   </head>
   <body>
   <nav class="navbar navbar-inverse">
     <div class="container">
       <div class="navbar-header">
-        <a class="navbar-brand" href="#">CIBlog</a>
+        <a class="navbar-brand" href=""<?php echo base_url(); ?>">CIBlog</a>
       </div>
       <div id="navbar">
         <ul class="nav navbar-nav">
           <li><a href="<?php echo base_url(); ?>">Home</a></li>
           <li><a href="<?php echo base_url(); ?>about">About</a></li>
+          <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
         </ul>
       </div>
     </div>
