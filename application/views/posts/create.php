2ca743f92e56d179aff82d4abe9c7433e64e3614
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-12-07 14:10:03 -0300

    adding images to posts

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,6 +1,6 @@
 <h2><?= $title; ?></h2>
 <?php echo validation_errors(); ?>
-<?php echo form_open('posts/create'); ?>
+<?php echo form_open_multipart('posts/create'); ?>
   <div class="form-group">
     <label>Title</label>
     <input type="text" class="form-control" name="title" placeholder="Add title">
@@ -16,6 +16,10 @@
         <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
       <?php endforeach; ?>
     </select>
+  </div>
+  <div class="form-group">
+    <label>Upload image</label>
+    <input type="file" name="userfile" size="20">
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
 </form>
