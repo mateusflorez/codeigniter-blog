13af45b8457743eda31e84d5500bc742390bd7b0
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-12-07 12:45:17 -0300

    posts index update

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,6 +2,6 @@
 <?php foreach($posts as $post) : ?>
   <h3><?php echo $post['title']; ?></h3>
   <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br>
-  <?php echo $post['body']; ?><br><br>
+  <?php echo word_limiter($post['body'], 50); ?><br><br>
   <p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read more</a></p>
 <?php endforeach; ?>
